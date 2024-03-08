bde0d993bc068fe473f1e238389fe5af9e00ce86
Luis Gabriel Mena Arnedo <lgm3na@gmail.com>
2024-03-08 12:24:39 +1100

    Agregar archivos de proyecto.

---
+++ GabrielButterFly.API/Controllers/SubtractionController.cs
@@ -0,0 +1,27 @@
+﻿using GabrielButterfly.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GabrielButterFly.API.Controllers
+{
+    [Route("GabrielButterFly/[controller]")]
+    [ApiController]
+    public class SubtractionController : ControllerBase
+    {
+        private readonly ISubtraction _subtraction;
+
+        public SubtractionController(ISubtraction subtraction)
+        {
+            _subtraction = subtraction;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetSubtractionResul(int num1, int num2)
+        {            
+            var result = await _subtraction.Subtraction(num1, num2);
+            return Ok(result);
+                      
+        }
+    }
+}
---
+++ GabrielButterfly.Core/Services/DivisionService.cs
@@ -0,0 +1,30 @@
+﻿using GabrielButterfly.Core.Entities;
+using GabrielButterfly.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabrielButterfly.Core.Services
+{
+    public class DivisionService : IDivision
+    {
+        public async Task<Result> Division(int num1, int num2)
+        {
+            Result result = new Result();
+
+            if (num2 == 0)
+            {
+                throw new InvalidOperationException("Divisor cannot be 0");
+            }
+            else
+            {
+                result.resutl = num1 / num2;
+            }
+
+            return result;
+
+        }
+    }
+}
